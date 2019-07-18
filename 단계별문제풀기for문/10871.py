ce9f3f50f1085a7a5c22f13bbf35dc8f495cbe96
myeongseok kwak <49023743+oh-bba-ya@users.noreply.github.com>
2019-07-19 01:33:19 +0900

    Create 10871.py

---
+++ 단계별문제풀기for문/10871.py
@@ -0,0 +1,8 @@
+N , X = map(int,input().split())
+A = list(map(int,input().split()))
+result = []
+for i in range(N):
+    if A[i] < X:
+        result.append(A[i])
+for i in result:
+    print(i,end = ' ')
