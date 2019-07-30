049f689c793b043dad36223a3c451751b1a47b40
myeongseok kwak <49023743+oh-bba-ya@users.noreply.github.com>
2019-07-30 23:50:01 +0900

    Update 2562.py

--- 단계별문제풀기1차원배열/2562.py
+++ 단계별문제풀기1차원배열/2562.py
@@ -1,7 +1,7 @@
 result = []
-for i in range(9):
-    num = int(input())
-    result.append(num)
+for i in range(9):          #반복문을 통해 입력값 받기.
+    num = int(input())          
+    result.append(num)      #입력값을 리스트에 추가 함으로써 max,index사용.
 max_result = max(result)
 index_result = int(result.index(max_result))+1
 print(max_result)
