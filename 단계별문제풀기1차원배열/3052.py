877f52d082f24ba01e952c41650ba011a009beaa
myeongseok kwak <49023743+oh-bba-ya@users.noreply.github.com>
2019-08-03 08:30:47 +0900

    Update 3052.py

--- 단계별문제풀기1차원배열/3052.py
+++ 단계별문제풀기1차원배열/3052.py
@@ -1,3 +1,5 @@
+#단순하게 입력값을 10개 받아서 하는방법.
+
 A =int(input())
 B =int(input())
 C =int(input())
@@ -25,3 +27,16 @@ for i in result:
     result2.append(N)
 s2=set(result2)
 print(len(s2))
+
+
+#반복문과 set함수로 쉽개 해결 할 수 있다.
+num_rest = []                 #입력받은 모든값의 나머지를 리스트화하기 위해 만듬.
+for i in range(10):            #일일히 한줄씩 입력을 받는것보단 반복문을 통하여 10개를 쉽게 받을 수 있다.
+    num = int(input())
+    rest = num % 42
+    num_rest.append(rest)
+set_rest = set(num_rest)
+print(len(set_rest))
+    
+
+
